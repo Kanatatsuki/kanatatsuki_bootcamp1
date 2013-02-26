53e5780f597d85fc78e0571bdde3c1dc5f13d2e6
kanatatsuki <ttm.stoat.hp@gmail.com>
2013-02-27 00:16:44 +0800

    files update

---
+++ development-template.php
@@ -0,0 +1,28 @@
+<? include("inc/header.php"); ?>
+<body>
+
+<!-- begin content -->
+
+<h1>Hello Header h1</h1>
+<h2>Hello Header h2</h2>
+<h3>Hello Header h3</h3>
+<h4>Hello Header h4</h4>
+<h5>Hello Header h5</h5>
+<h6>Hello Header h6</h6>
+
+<p>Paragraph: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue ipsum, tristique at dapibus non, facilisis ut orci. Nam id laoreet neque. In sit amet dolor lorem. Ut vitae risus tortor. Donec ac mattis est. Nunc sit amet diam in sem venenatis egestas nec eget neque. Nulla ornare ipsum sed lorem sollicitudin dapibus. Integer id nisi a mi auctor porta. Donec purus neque, convallis id aliquam in, tempor a nisl. Nunc pretium sem porta nibh vestibulum in ornare tellus tincidunt. Nam quis dolor a leo varius feugiat. Nullam augue sapien, adipiscing ac sagittis eu, auctor et diam. In et convallis velit. Aliquam porttitor ullamcorper lectus et porttitor. </p>
+
+<a href="#">A link to see the color</a>
+
+<blockquote>blockquote: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue ipsum, tristique at dapibus non, facilisis ut orci. Nam id laoreet neque.</blockquote>
+
+<ul>
+	<li>Here is my list 1</li>
+    <li>Here is my list 2</li>
+    <li>Here is my list 3</li>
+    <li>Here is my list 4</li>
+</ul>
+
+
+<? include("inc/analytics.php"); ?>
+<? include("inc/footer.php"); ?>
---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<?
+/****************************************
+	Template Foundation
+	Written By: Richard Chiriboga
+	For: THAT Agency
+****************************************/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>DEVELOPMENT TEMPLATE</title>
+<link rel="stylesheet" media="all" type="text/css" href="/css/reset-960.css" />
+<link rel="stylesheet" media="all" type="text/css" href="/css/site.css" />
+<script type="text/javascript" src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="/js/site.js"></script>
+</head>
